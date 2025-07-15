693185b2d57db83896c79b4087620aa3aacbf6d6
Roni <ronimalamud@gmail.com>
2025-07-15 16:39:20 -0300

    Creacion primer formulario

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Intensivo2025_Grupo4
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            // This is the main entry point for the application.
+            // You can add your application logic here.
+            Console.WriteLine("Hello, World!");
+            // Keep the console window open until a key is pressed.
+            Console.ReadKey();
+        }
+    }
+}
