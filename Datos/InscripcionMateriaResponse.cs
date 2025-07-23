0eeeb6a40459da61390f85a5c4fa33335d3239fb
Ivo Lise <ivolucaslise@gmail.com>
2025-07-23 15:41:29 -0300

    inscripcion persistencia

---
+++ Datos/InscripcionMateriaResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class InscripcionMateriaResponse
+    {
+        public int id {  get; set; }
+        public string nombre { get; set; }
+        public string condicion { get; set; }
+
+        public int nota { get; set; }
+
+    }
+    // Para inscribir materias (POST) no hace falta clase, es un List<long> directamente.
+}
---
+++ Persistencia/InscripcionPersistencia.cs
@@ -0,0 +1,45 @@
+﻿using Persistencia.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Persistencia
+{
+    public class InscripcionPersistencia
+    {
+        // Obtener materias en las que el alumno está inscripto
+        public List<InscripcionMateriaResponse> ObtenerMateriasInscriptas(long alumnoId)
+        {
+            HttpResponseMessage response = WebHelper.Get($"tpIntensivo/alumno/{alumnoId}/materias");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                var materias = JsonSerializer.Deserialize<List<InscripcionMateriaResponse>>(json);
+                return materias;
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                throw new Exception("No se pudieron obtener las materias inscriptas.");
+            }
+        }
+
+        // Inscribir al alumno en una lista de materias (array de IDs)
+        public void InscribirMaterias(long alumnoId, List<long> idsMaterias)
+        {
+            string json = JsonSerializer.Serialize(idsMaterias);
+            HttpResponseMessage response = WebHelper.Post($"tpIntensivo/alumno/{alumnoId}/materias", json);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                throw new Exception("No se pudieron inscribir las materias.");
+            }
+        }
+    }
+}
